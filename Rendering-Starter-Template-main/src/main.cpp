783f965a579ebc8e2852508830ca1b55431e31df
Phylip3 <martin.revolbuisson@gmail.com>
2025-01-23 15:13:51 +0100

    Shaders

--- Rendering-Starter-Template-main/src/main.cpp
+++ Rendering-Starter-Template-main/src/main.cpp
@@ -6,8 +6,32 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
+    auto const rectangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0}},
+            .data   = {
+                -0.5f, -0.5f, // Position2D du 1er sommet
+                +0.5f, -0.5f, // Position2D du 2ème sommet
+                +0.5f, +0.5f, // Position2D du 3ème sommet
+                -0.5f, +0.5f  // Position2D du 4ème sommet
+            },
+        }},
+        .index_buffer   = {
+            0, 1, 2, // Indices du premier triangle : on utilise le 1er, 2ème et 3ème sommet
+            2, 3, 0  // Indices du deuxième triangle : on utilise le 1er, 3ème et 4ème sommet
+        },
+    }};
+
+    auto const shader = gl::Shader{{
+        .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
+        .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
+    }};
+
     while (gl::window_is_open())
     {
-        // Rendu à chaque frame
+        glClearColor(0.f, 0.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        gl::bind_default_shader();
+        rectangle_mesh.draw();
     }
 }
